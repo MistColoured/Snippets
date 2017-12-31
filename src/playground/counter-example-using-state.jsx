758cac3bf26d4036897c1bcb708cc74ff6618616
Michael Allen <mike@mistColoured.com>
2017-12-31 12:16:45 +0000

    Practicing

--- src/playground/counter-example-using-state.jsx
+++ src/playground/counter-example-using-state.jsx
@@ -1,35 +1,3 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class CounterInterview extends Component {
-    state = {
-      count: 107,
-    };
-  handlePlus = () => {
-    this.setState((prevState) => ({
-      count: prevState.count + 1,
-    }));
-  }
-  handleMinus = () => {
-    this.setState((prevState) => ({
-      count: prevState.count - 1,
-    }));
-  }
-  handleReset = () => {
-    this.setState(() => ({
-      count: 107,
-    }));
-  }
-  render() {
-    return (
-      <div>
-        Count: {this.state.count}
-        <button onClick={this.handlePlus}>+</button>
-        <button onClick={this.handleMinus}>-</button>
-        <button onClick={this.handleReset}>Reset</button>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<CounterInterview />, document.getElementById('app'));

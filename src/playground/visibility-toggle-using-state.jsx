8d17233ebc5179a79aa1d5cf0e8d78d5de1ff1c0
Michael Allen <mike@mistColoured.com>
2017-12-31 12:42:58 +0000

    Practice finished

--- src/playground/visibility-toggle-using-state.jsx
+++ src/playground/visibility-toggle-using-state.jsx
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class VisibilityToggleInterview extends Component {
-  state = {
+  constructor(props) {
+    super(props);
+    this.handleToggle = this.handleToggle.bind(this);
+    this.state = {
       visibility: false,
     };
-  handleToggle = () => {
+  }
+  handleToggle() {
     this.setState((prevState) => ({
       visibility: !prevState.visibility,
-    }))
-  };
+    }));
+  }
   render() {
     return (
       <div>
+        <h1>Visibility Interview</h1>
         <button onClick={this.handleToggle}>{this.state.visibility ? 'Hide' : 'Show'}</button>
         {this.state.visibility && <p>ZZ Top</p>}
       </div>

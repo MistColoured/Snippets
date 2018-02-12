f9de702001efeddd5757f32195b10f21a91d6f01
Michael Allen <mike@mistColoured.com>
2018-02-12 17:24:28 +0000

    Practice

--- src/playground/ToggleVisibility.jsx
+++ src/playground/ToggleVisibility.jsx
@@ -5,20 +5,18 @@ class ToggleVisibility extends Component {
     super(props);
     this.handleToggle = this.handleToggle.bind(this);
     this.state = {
-      visibility: true,
+      visibility: false,
     };
   }
   handleToggle() {
     this.setState(prevState => ({
       visibility: !prevState.visibility,
-    }));
+    }))
   }
   render() {
     return (
       <div>
-        <button onClick={this.handleToggle}>
-          {this.state.visibility ? 'Hide' : 'Show'}
-        </button>
+        <button onClick={this.handleToggle}>{this.state.visibility ? 'Hide' : 'Show'}</button>
         {this.state.visibility && <p>Something secret</p>}
       </div>
     );

81ec2e9f3d2e1cbaec1f8ee60ba765f19968e2f5
Michael Allen <mike@mistColoured.com>
2018-07-30 17:45:00 +0100

    Swapped to ToDo.

--- Practice/ErrorWrapper.jsx
+++ Practice/ErrorWrapper.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import ErrorBoundary from './ErrorBoundary';
-import DisplayComponent from './Clock';
+import DisplayComponent from './ToDo';
 
 const ErrorWrapper = () => (
   <ErrorBoundary>

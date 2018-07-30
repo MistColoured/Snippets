73c762d9353d7463ef3301fabc41b9f2a87e9e41
Michael Allen <mike@mistColoured.com>
2018-07-30 17:44:22 +0100

    Practice.

--- Practice/ToggleVisibility.jsx
+++ Practice/ToggleVisibility.jsx
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import { Div, Paragraph } from './SomeStyle';
+// import { Div, Paragraph } from './SomeStyle';
 
 class ToggleVisibility extends Component {
   state = {
     visible: false,
   }
+
   handleToggle = () => {
-    this.setState((prevState) => ({
+    this.setState(prevState => ({
       visible: !prevState.visible,
     }));
   }
+
   render() {
+    const { visible } = this.state;
+    const content = visible ? (
+      <div>
+        Hello
+        <button onClick={this.handleToggle}>I am a button</button>
+      </div>) : <div>Goodbye</div>;
     return (
       <div>
-        <button onClick={this.handleToggle}>Click Me</button>
-        {this.state.visible ? 'True' : 'False'}
-        {this.state.visible && <p>Top Secret</p>}
+        <button onClick={this.handleToggle}>
+          Show the content
+        </button>
+        {content}
       </div>
     );
   }
